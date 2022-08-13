c4ccaf188270129c890b4f3290bb2b1a95fc348e
VolkSeriy <volkseriy@VolkSeriys-MacBook-Air.local>
2022-08-13 22:08:18 +0300

    bug fixed

--- poll.py
+++ poll.py
@@ -5,9 +5,9 @@ age = input('print your age')
 question = input('do you now who are me?')
 if question.lower == 'yes':
     print('YOU ARE RUSSIAN SPY')
-elif yourname[0] == 'A' or yourname[0] == 'L' or yourname[0] == 'O':
+elif yourname[0].lower == 'A' or yourname[0].lower == 'L' or yourname[0].lower == 'O':
         print('YOU ARE ITALIANO MAFIA')
-elif secondname[0] == 'A' or secondname[0] == 'U':
+elif secondname[0].lower == 'A' or secondname[0].lower == 'U':
         print('YOU ARE A DOCTOR')
 elif int(age) < 18:
             print('YOU ARE CIVILIAN')
